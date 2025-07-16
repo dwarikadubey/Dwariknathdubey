f33c85d275ae1506b6c4855ffcdf9cf5b4ae3181
unknown <dwarika0802@gmail.com>
2025-07-16 21:01:44 +0530

    Second commit

--- zigwheelsdemo-main/cucumber.js
+++ zigwheelsdemo-main/cucumber.js
@@ -1,3 +1,3 @@
 module.exports = {
-    default: "--require tests/world.ts --require tests/usedPopularModels.steps.ts --require-module ts-node/register"
+    default: "--require tests/world.ts --require tests/usedPopularModels.steps.ts --require tests/hondaBikesUnder4L.steps.ts --require-module ts-node/register"
 };
